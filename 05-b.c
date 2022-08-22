ad966b5793a7c6274a4df6a1126b4bb85107d0c1
Amit Kumar <83487500+amitkrxyz@users.noreply.github.com>
2022-08-22 12:21:59 +0530

    05-b

--- 05-b.c
+++ 05-b.c
@@ -2,32 +2,40 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <ctype.h>
 
-int square_root(int n)
+float square_root(int n)
 {
     if (n < 0)
     {
         return -1;
     }
-
+    float result;
     for (int i = 0; i < n; i++)
     {
         if (i * i == n)
         {
-            return i;
+            result = i;
+            break;
         }
         else if (i * i > n)
         {
-            return i - 1;
+            result = i - 1;
+            break;
         }
     }
-}
 
-double square_root2(int n)
-{
-    double result = pow(10, log10(n) / 2);
-    return result;
+    for (float i = 0; i < 1; i += 0.00000000001)
+    {
+        result += i;
+        if (result * result == n)
+        {
+            return result;
+        }
+        else if (result * result > n)
+        {
+            return result - i;
+        }
+    }
 }
 
 int main(void)
@@ -35,7 +43,7 @@ int main(void)
     int n;
     printf("Number: ");
     scanf("%i", &n);
-    int sq_root = square_root(n);
+    float sq_root = square_root(n);
     if (sq_root < 0)
     {
         printf("Error negative number...\n");
